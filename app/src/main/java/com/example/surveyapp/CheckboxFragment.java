a0eaa7c266cfa4e384726d2eba5020f8ee9aad43
kucse160235 <m.afnan2016@gmail.com>
2020-08-20 03:53:21 +0600

    Fixed CheckBox and Number Fragment
    1. Primary Testing completed
    2. Still local database is not created

--- app/src/main/java/com/example/surveyapp/CheckboxFragment.java
+++ app/src/main/java/com/example/surveyapp/CheckboxFragment.java
@@ -9,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +22,10 @@ import android.view.ViewGroup;
 public class CheckboxFragment extends Fragment {
 
     private static final String TAG = "CheckboxFragment";
+
+    TextView textViewQuestionCheckbox;
+    CheckBox checkBoxYes,checkBoxNo;
+    Button btnViewContinueNextCheckbox;
 
     private Question question;
 
@@ -31,6 +39,30 @@ public class CheckboxFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_checkbox, container, false);
+
+        textViewQuestionCheckbox = view.findViewById(R.id.text_view_question_checkbox);
+        checkBoxYes = view.findViewById(R.id.checkBoxYes);
+        checkBoxNo = view.findViewById(R.id.checkBoxNo);
+        btnViewContinueNextCheckbox = view.findViewById(R.id.btn_continue_next_checkbox);
+
+        textViewQuestionCheckbox.setText(question.getTitle());
+        btnViewContinueNextCheckbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkBoxYes.isChecked() || checkBoxNo.isChecked()){
+                    if (checkBoxYes.isChecked()){
+                        Toast.makeText(getContext(), "Yes", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getContext(), "No", Toast.LENGTH_SHORT).show();
+                    }
+                    SurveyActivity surveyActivity = (SurveyActivity) getActivity();
+                    surveyActivity.showNextQuestion();
+                }else{
+                    Toast.makeText(getContext(), "Please select an answer", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         return view;
     }
 }
--- app/src/main/java/com/example/surveyapp/NumberFragment.java
+++ app/src/main/java/com/example/surveyapp/NumberFragment.java
@@ -9,15 +9,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NumberFragment#newInstance} factory method to
+ * Use the {@link NumberFragment} factory method to
  * create an instance of this fragment.
  */
 public class NumberFragment extends Fragment {
 
     private static final String TAG = "NumberFragment";
+
+    private TextView textViewQuestionNumber;
+    private Button btnContinueNextNumber;
+    private EditText editTextNumber;
 
     private Question question;
 
@@ -30,12 +38,30 @@ public class NumberFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_number, container, false);
+        View view = inflater.inflate(R.layout.fragment_number, container, false);
+
+        textViewQuestionNumber = view.findViewById(R.id.text_view_question_number);
+        editTextNumber = view.findViewById(R.id.editText_number);
+        btnContinueNextNumber = view.findViewById(R.id.btn_continue_next_number);
+
+        textViewQuestionNumber.setText(question.getTitle());
+
+        btnContinueNextNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number = editTextNumber.getText().toString();
+                if (number.matches("")){
+                    Toast.makeText(getContext(), "You have to enter your number first.", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getContext(), "Number :"+number, Toast.LENGTH_SHORT).show();
+                    SurveyActivity surveyActivity = (SurveyActivity) getActivity();
+                    surveyActivity.showNextQuestion();
+                }
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-    }
 }
